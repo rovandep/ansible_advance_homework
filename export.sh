f768657ad576fda600a1c8bb1531c7323117ab6b
romuald <romuald@redhat.com>
2019-11-07 16:14:43 -0800

    homework lab

--- export.sh
+++ export.sh
@@ -1,8 +1,8 @@
-TOWER_GUID=2c3b
-OSP_GUID=414d
-OPENTLC_LOGIN=rovandep-redhat.com
-OPENTLC_PASSWORD=
-GITHUB_REPO=https://github.com/rovandep/ansible_advance_homework.git
-JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
-REGION=
-RH_MAIL_ID=rovandep@redhat.com
+export TOWER_GUID=2c3b
+export OSP_GUID=414d
+export OPENTLC_LOGIN=rovandep-redhat.com
+export OPENTLC_PASSWORD=
+export GITHUB_REPO=https://github.com/rovandep/ansible_advance_homework.git
+export JQ_REPO_BASE=http://www.opentlc.com/download/ansible_bootcamp
+export REGION=
+export RH_MAIL_ID=rovandep@redhat.com
